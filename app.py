ad6e9f6ab0d26d7ce63e12a01374707d33793b36
hushunxin <llyzhsx@163.com>
2019-03-24 12:26:10 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
+name = 'hushunxin'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+'''
+    @app.route('/')
+    @app.route('/hello')
+    @app.route('/index')
+    def hello():
+        return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+    @app.route('/user/<name>')
+    def user_page(name):
+        return f'User: {name}'
+
+    @app.route('/test')
+    def test_url_for():
+        print(url_for('hello'))
+        print(url_for('user_page', name='hushunxin'))
+        print(url_for('user_page', name='胡顺新'))
+        print(url_for('test_url_for'))
+        print(url_for('test_url_for', num=2))
+        return 'Test page'
+'''
+
 @app.route('/')
-@app.route('/hello')
-@app.route('/index')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {name}'
-
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='hushunxin'))
-    print(url_for('user_page', name='胡顺新'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
+def index():
+    return render_template('/index.html', name=name, movies=movies)
